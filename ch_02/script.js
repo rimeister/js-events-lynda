ebee2df8ab92af820bbc65c0e39c0a281a45bc95
Riley Boyd <contact@rileyboyd.com>
2018-05-18 10:35:37 -0400

    Chapter 2: Created my own solution that works, gonna look at his solution now

--- ch_02/script.js
+++ ch_02/script.js
@@ -1,13 +1,29 @@
 document.querySelector('.grid').addEventListener('click',function(e){
 
-	var removeTarget = e.target.parentNode; 
-	/* The target of the click is the image, but we 
-	want the parent of the image, the <li> */
+	var howMany = this.querySelectorAll('li').length;
 
-	/* So, we have to taget the parent of the <li> (the <ul>) so that 
-	we can use the removeChild() method to remove the <li> */
-	/* It's a circular way of doing things, but that's what he says to do */
-	removeTarget.parentNode.removeChild(removeTarget);	
+	if (e.target.tagName === 'IMG') {
+
+		if (howMany > 1) {
+
+			var removeTarget = e.target.parentNode; 
+			/* The target of the click is the image, but we 
+			want the parent of the image, the <li> */
+
+			/* So, we have to taget the parent of the <li> (the <ul>) so that 
+			we can use the removeChild() method to remove the <li> */
+			/* It's a circular way of doing things, but that's what he says to do */
+			removeTarget.parentNode.removeChild(removeTarget);
+
+		}
+	
+	}
+
+	if (howMany < 3) {
+
+		document.querySelector('#art p').innerHTML = document.querySelector('.grid li img').alt;
+
+	}
 
 
 },false);
