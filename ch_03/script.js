1feacb007f8b413d3e570a3ff18c069199210262
Riley Boyd <contact@rileyboyd.com>
2018-05-21 11:23:07 -0400

    Working on loading spinner tutorial, chapter 3 video 1

---
+++ ch_03/script.js
@@ -0,0 +1,17 @@
+document.querySelector('img.preview').addEventListener('click',function(e){
+	
+	var lowRes = e.target.src;
+	var myOverlay = document.querySelector('.overlay');
+	var highRes = document.createElement('img');
+	var mySpinner = document.createElement('img');
+
+	myOverlay.style.display = 'block';
+	highRes.class = 'bgImg';
+	highRes.src = lowRes.substr(0,lowRes.length-7) + '.jpg'; // This takes the low res src, removes the last seven characters, adds .jpg back on
+	myOverlay.appendChild(highRes);
+
+	mySpinner.className = 'spinner';
+	mySpinner.src = 'images/spinner.gif';
+	myOverlay.appendChild(mySpinner);
+
+}, false);
