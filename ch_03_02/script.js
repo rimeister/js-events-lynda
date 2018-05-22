d7b4fa58503430334f079e0cd04594ffe0e6b627
Riley Boyd <contact@rileyboyd.com>
2018-05-22 11:27:54 -0400

    Added comment explaining .getAttribute() method, used className for .playing and .paused

--- ch_03_02/script.js
+++ ch_03_02/script.js
@@ -1,8 +1,9 @@
 var jukeBox = document.querySelector('ul.player');
 jukeBox.addEventListener('click', function(e){
 	var audioPlayer = document.createElement('audio');
-	var songName = e.target.getAttribute('data-src');
+	var songName = e.target.getAttribute('data-src'); // Because data- attribute is custom attribute, we have to use .getAttribute()
 	audioPlayer.id = 'player';
+	e.target.className = 'playing';
 	audioPlayer.src = songName;
 	document.body.appendChild(audioPlayer);
 	audioPlayer.play();
